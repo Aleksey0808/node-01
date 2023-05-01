40e3d5d7353e11ab94ddc9bd5204bdb07ec90f95
Aleksey <103374981+Aleksey0808@users.noreply.github.com>
2023-05-01 16:05:45 +0300

    010523

--- db/index.js
+++ db/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs/promises')
 const path = require('path')
+const {nanoid} = require('nanoid')
 
 const contactPath = path.join(__dirname, 'contacts.json')
 
@@ -9,12 +10,52 @@ const getAll = async () => {
 }
 
 const getById = async (id) => {
+  const contactId = String(id)
   const contacts = await getAll()
-  const result = contacts.find((contact) => contact.id === id)
+  const result = contacts.find((contact) => contact.id === contactId)
   return result || null
+}
+
+const addContact = async (data) => {
+  const contacts = await getAll()
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  }
+  contacts.push(newContact)
+  await fs.writeFile(contactPath, JSON.stringify(contacts, null, 2))
+  return newContact
+}
+
+const updateContact = async (id, date) => {
+  const contactId = String(id)
+  const contacts = await getAll()
+  const index = contacts.findIndex(item => item.id === id)
+  if (index === -1) {
+    return null
+  }
+  contacts[index] = {id, ...date}
+  await fs.writeFile(contactPath, JSON.stringify(contacts, null, 2))
+  return contacts[index]
+}
+
+const deleteContact = async (id) => {
+  const contactId = String(id)
+  const contacts = await getAll()
+  const index = contacts.findIndex(item => item.id === id)
+  if (index === -1) {
+    return null
+  }
+  const [result] = contacts.splice(index, 1)
+  await fs.writeFile(contactPath, JSON.stringify(contacts, null, 2))
+  return result
 }
 
 module.exports = {
   getAll,
   getById,
+  addContact,
+  updateContact,
+  deleteContact
 }
+
